2735fbaa74bd9f285b9978dd8573f9fedb939351
julikss <julie.lehenka@gmail.com>
2022-06-20 12:53:52 +0300

    Update write to db func

--- src/services/add.js
+++ src/services/add.js
@@ -1,13 +1,14 @@
 'use strict';
 const fs = require('fs');
+const path = '../../db/db.json';
 
 const args = process.argv;
-const path = '../../db/db.json';
 
 if (!fs.existsSync(path)) {
   let createStream = fs.createWriteStream(path);
   createStream.end();
 }
+
 
 const menuFunction = () => {
   const menu = `
@@ -17,43 +18,47 @@ const menuFunction = () => {
   console.log(menu);
 };
 
-const addFunction = () => {
+
+const getArgs = () => {
   let newTodo = '';
 
-  for (let i = 3; i < args.length; i++) {
-    if (args[i].includes(',')) {
-      for (let j = 3; j < i+1; j++) {
-        newTodo += args[j] + ' ';
-      }
-      const task = newTodo.slice(0, -2).toString();
-      const deadline = args[i+1];
-
-      if (task) {
-        const taskDescription = {
-          task: task,
+    for (let i = 3; i < args.length; i++) {
+      if (args[i].includes(',')) {
+        for (let j = 3; j < i+1; j++) {
+          newTodo += args[j] + ' ';
+        }
+        const taskName = newTodo.slice(0, -2).toString();
+        const deadline = args[i+1];
+  
+        const taskObj = {
+          id: 0,
+          task: taskName,
           deadline: deadline,
           progress: 'undone'
         }
 
-        const jsonString = fs.readFileSync(path).toString();
-
-        const fileObj = {
-           id: taskDescription
-        }
-        console.log(fileObj);
-        fs.writeFile(path, JSON.stringify(fileObj) + '\n' + jsonString, (err) => {
-          if (err) throw err;
-        });
-
-        } else {
-          console.log('No task added');
-        }
+        return taskObj;
+      } 
     }
-  }
+}
+
+
+const addFunction = () => {
+  const task = getArgs();
+  const db = require(path);
+  db.counter ++;
+  task.id = db.counter;
+  db.todos.push(task);
+          
+  fs.writeFileSync(path, JSON.stringify(db), (err) => {
+    if (err) throw err;
+  });
+
+  console.log('Task is added');
 }
 
 
 if (args[2] === 'add') {
- //menuFunction();
+  //menuFunction();
   addFunction();
 }
