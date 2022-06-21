2d91e8744ed55c0239a2361024220e01f1119e47
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-06-22 00:19:20 +0200

    Added method for task delete

---
+++ src/services/delete.js
@@ -0,0 +1,29 @@
+'use strict';
+const fs = require('fs');
+const path = '../../db/db.json';
+const { getArgs } = require('../helpers/getArgs')
+const db = require(path);
+
+if (!fs.existsSync(path)) {
+  let createStream = fs.createWriteStream(path);
+  createStream.end();
+}
+
+const deleteTask = (args) => {
+  const task = getArgs(args);
+  let taskIndex;
+  for(let el of db.todos) {
+    if(task.task == el.task && task.deadline == el.deadline) {
+      taskIndex = db.todos.indexOf(el)
+      db.todos.splice(taskIndex,1)
+      db.counter--;
+
+      fs.writeFileSync(path, JSON.stringify(db), (err) => {
+        if (err) throw err;
+      });
+      console.log('Task was deleted:\n', task);
+    }
+  }
+}
+
+module.exports = deleteTask;
--- src/services/main.js
+++ src/services/main.js
@@ -4,6 +4,7 @@ const args = process.argv;
 const addFunction = require('./addTask');
 const addDescription = require('./addDescription');
 const showAllTask = require('./showAllTask');
+const deleteTask = require('./delete');
 
 // node main.js add <todo, deadline(YYYY-MM-DD)>   # Add a new todo
 // node main.js describe <id> <description>   # Add description to a todo
@@ -15,7 +16,9 @@ const runTodoList = () => {
         addDescription(args);
     }  else if (args[2] === 'ls'){
         showAllTask(args)
-    }
+    } else if (args[2] === 'delete'){
+      deleteTask(args)
+  }
 }
 
 runTodoList();
