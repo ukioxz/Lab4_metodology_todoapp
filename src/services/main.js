f14301ce0749aede53e3ada5ad6cfae4220292a2
julikss <julie.lehenka@gmail.com>
2022-06-23 20:13:10 +0300

    Add console menu

--- src/services/main.js
+++ src/services/main.js
@@ -10,20 +10,12 @@ const showOverdue = require('./showOverdue');
 const showSortDeadline = require('./sortDeadline');
 const showReportTodo = require('./reportTodo');
 const updateTask = require('./update');
+const { showMenu } = require('../helpers/consoleMenu')
 
 const commands = ['add', 'describe', 'ls', 'delete', 'done', 'showOverdue', 
                   'undone', 'report', 'update'];
 const methods = [addFunction, addDescription, showAllTask, deleteTask, markAsDone, 
                   showOverdue, showSortDeadline, showReportTodo, updateTask];
-
-
-// node main.js add <todo: deadline(YYYY-MM-DD)>   # Add a new todo
-// node main.js describe <id> <description>   # Add description to a todo
-// node main.js delete <taskName> <date(YYYY-MM-DD)>   # Delete task
-// node main.js done <id> *<date(YYYY-MM-DD)>   # Mark task as done *optional
-// node main.js update title <id> <name> # Update task title
-// node main.js update description <id> <description> # Update description
-// node main.js update deadline <id> <deadline(YYYY-MM-DD)> # Update deadline
 
 const runTodoList = () => {
     for(let el of commands) {
@@ -31,4 +23,5 @@ const runTodoList = () => {
     }
 }
 
+showMenu();
 runTodoList();
