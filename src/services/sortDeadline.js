8b6a3b342a0eeba3fea86e0f6b4f095d51c7e062
ukioxz <ikatember@gmail.com>
2022-06-23 09:32:55 +0300

    Add feature show not overdue tasks

--- src/services/sortDeadline.js
+++ src/services/sortDeadline.js
@@ -7,18 +7,29 @@ const { getArgs } = require('../helpers/getArgs')
 const showSortDeadline = (args) => {
   const task = getArgs(args);
   
+  let date = new Date();
+  let dateStr = date.toISOString().split('T')[0];
+  
   const dataJson = data.todos;
   const dataStr = JSON.stringify(dataJson);
   const dataTodos = JSON.parse(dataStr);
-  
-  const undoneTodos = dataTodos.filter(el => el.progress === "undone");
 
-  const sortByDate = undoneTodos.sort((a, b) => new Date(a.deadline - b.deadline));
+  const undoneTodos = dataTodos.filter(el => el.progress === "undone");
+  const sortByDate = undoneTodos.sort((a, b) => {
+    let dateA = new Date(a.deadline);
+    let dateB = new Date(b.deadline);
+    return dateA-dateB;
+  });
  
   for (let i = 0; i < sortByDate.length; i++)
   {
+    let sortedTodos = [];
     const todo = sortByDate[i];
-    console.log(`${todo.task}: ${todo.description} ${todo.deadline} ${todo.progress}`);
+    const todosDeadline = sortedTodos.push(`${todo.deadline}`)
+    let todosStr = sortedTodos.join();
+    if (dateStr <= todosStr) {
+      console.log(`${todo.task}: ${todo.description} ${todo.deadline} ${todo.progress}`);
+    }
   }
 }
 
