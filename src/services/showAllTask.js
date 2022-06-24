3ee1ceddb9bb3ffc3ff61403e2f61bf792e64ee9
ukioxz <ikatember@gmail.com>
2022-06-24 09:19:44 +0300

    Change output showAll function to table

--- src/services/showAllTask.js
+++ src/services/showAllTask.js
@@ -1,21 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-const data = require('../../db/db.json');
-const { getArgs } = require('../helpers/getArgs')
+const db = require('../../db/db.json');
 
 const showAllTask = (args) => {
-  const task = getArgs(args);
-  const dataJson = data.todos;
-  const dataStr = JSON.stringify(dataJson)
-  const dataTodos = JSON.parse(dataStr)
-
-  for (let i = 0; i < dataTodos.length; i++)
-  {
-    const todo = dataTodos[i];
-    console.log(`${todo.id}:${todo.task} ${todo.description} ${todo.deadline} ${todo.progress}`);
-  }
+  const data = db.todos;
+  const tableData = data.reduce((acc, {id, ...x}) => { acc[id] = x; return acc}, {});
+  console.table(tableData);
 }
 
 module.exports = showAllTask
-
